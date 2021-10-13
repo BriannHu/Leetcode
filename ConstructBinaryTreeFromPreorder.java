b9263ca13b34d03dc82ae9cdc184029150fb332e
Brian Hu <blp.hu5@gmail.com>
2021-10-12 21:49:56 -0400

    ConstructBinaryTreeFromPreorder

---
+++ ConstructBinaryTreeFromPreorder.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+public class ConstructBinaryTreeFromPreorder {
+
+     public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+             this.val = val;
+             this.left = left;
+             this.right = right;
+        }
+     }
+
+     public TreeNode ConstructBinaryTreeFromPreorder(int[] preorder) {
+         if (preorder.length == 0 || preorder == null) return null;
+         Stack<TreeNode> stack = new Stack<TreeNode>();
+         TreeNode root = new TreeNode(preorder[0]);
+         stack.push(root);
+
+         for (int i=1; i<preorder.length; i++) {
+             TreeNode current = new TreeNode(preorder[i]);
+             if (current.val < stack.peek().val) {
+                 stack.peek().left = current;
+             } else {
+                 TreeNode parent = stack.peek();
+                 while (!stack.isEmpty() && current.val > stack.peek().val) {
+                     parent = stack.pop();
+                 }
+                 parent.right = current;
+             }
+
+             stack.push(current);
+         }
+         return root;
+    }
+
+}

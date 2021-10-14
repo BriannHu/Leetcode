1b47613f196d13ac5eaf52d8490cd03e5bb29b57
Brian Hu <blp.hu5@gmail.com>
2021-10-13 23:15:23 -0400

    Added PerfectSquares

---
+++ PerfectSquares.java
@@ -0,0 +1,18 @@
+public class PerfectSquares {
+
+    public int PerfectSquares(int n) {
+        int[] dp = new int[n+1];
+
+        for (int i=1; i<=n; i++) {
+            int j = 1;
+            int min = Integer.MAX_VALUE;
+            while (i - j*j >= 0) {
+                min = Math.min(min, dp[i-j*j]+1);
+                j++;
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+
+}

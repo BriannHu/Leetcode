8c5a0107eaf0a8cf776be9e48fbc222b60920242
Brian Hu <blp.hu5@gmail.com>
2021-10-15 17:07:46 -0400

    Added difficulty folder

--- sort_problems.sh
+++ sort_problems.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-#problems=
+# move all java files (if any exist) from root to all_problems
+count=`ls -1 *.java 2>/dev/null | wc -l`
+if [ $count != 0 ]
+then
+	echo "New Java files found, moving to central folder"
+	mv *.java all_problems/
+fi
 
-mv *.java all_problems/
-
+# extract difficulty from each java file
+# convert to lowercase and copy into corresponding folder
+for file in all_problems/*; do
+	difficulty=`head -1 $file | cut -d' ' -f2`
+	lowercase=`echo $difficulty | awk '{print tolower($0)}'`
+	cp $file by_difficulty/$lowercase/ 
+	echo "Copied $file into by_difficulty/$lowercase/"
+done
 
 
 echo "Done sorting"

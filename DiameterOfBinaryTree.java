3165baafeb4c23699ee8643072b26a760fd3b07f
Brian Hu <blp.hu5@gmail.com>
2021-10-17 18:33:53 -0400

    Updated description

--- DiameterOfBinaryTree.java
+++ DiameterOfBinaryTree.java
@@ -1,19 +1,19 @@
 // EASY - https://leetcode.com/problems/diameter-of-binary-tree/
 
-class TreeNode {
-     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode() {}
-     TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
 public class DiameterOfBinaryTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     int max_so_far = 0;
 

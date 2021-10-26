7543d9d7983075b6102431d50f20493331923496
Brian Hu <blp.hu5@gmail.com>
2021-10-25 20:16:48 -0400

    Added InvertBinaryTree

---
+++ InvertBinaryTree.java
@@ -0,0 +1,29 @@
+// EASY - https://leetcode.com/problems/invert-binary-tree/
+
+public class InvertBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = invertTree(right);
+        root.right = invertTree(left);
+        return root;
+    }
+
+}

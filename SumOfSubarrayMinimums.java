f9d243363a564e83b0d86c7d72ed22da99f64ae2
Brian Hu <blp.hu5@gmail.com>
2021-11-11 22:38:17 -0500

    Added SumOfSubarrayMinimums

---
+++ SumOfSubarrayMinimums.java
@@ -0,0 +1,39 @@
+// MEDIUM - https://leetcode.com/problems/sum-of-subarray-minimums/
+
+import java.util.Stack;
+
+public class SumOfSubarrayMinimums {
+
+    public int sumSubarrayMins(int[] arr) {
+        // initialize previous less and next less element of each element in array
+        Stack<int[]> previousLess = new Stack<>();
+        Stack<int[]> nextLess = new Stack<>();
+        int[] leftDistance = new int[arr.length];
+        int[] rightDistance = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            while (!previousLess.isEmpty() && previousLess.peek()[0] >= arr[i]) {
+                previousLess.pop();
+            }
+
+            leftDistance[i] = previousLess.isEmpty() ? i+1 : i - previousLess.peek()[1];
+            previousLess.push(new int[]{arr[i], i});
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!nextLess.isEmpty() && nextLess.peek()[0] > arr[i]) {
+                nextLess.pop();
+            }
+            rightDistance[i] = nextLess.isEmpty() ? arr.length-i : nextLess.peek()[1] - i;
+            nextLess.push(new int[]{arr[i], i});
+        }
+
+        long total = 0;
+        long mod = (long)1e9 + 7;
+        for (int i = 0; i < arr.length; i++) {
+            total = (total + (long)arr[i] * leftDistance[i] * rightDistance[i]) % mod;
+        }
+        return (int)total;
+    }
+
+}
